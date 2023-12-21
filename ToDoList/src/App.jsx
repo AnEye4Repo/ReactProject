79468959d32d6072f47b48c72ad17fa0272b510f
Juan Cordoba <juanzinacordoba@gmail.com>
2023-12-21 01:09:37 -0500

    Trying to finish App.jsx

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import TodoList from './List/TodoList';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  // const [todos,settodos] = useState([]);
+  // const [newTodo,setnewTodo] = useState('');
+
+  // const addTodoHandler = (newTodo) =>{
+  //   if (newTodo.trim()!== ''){
+  //     const newTodoItem = {
+  //       id: new Date().getTime(),
+  //       Text: newTodo,
+  //       completed: false,
+  //     };
+  //     settodos((prevTodos) => [...prevTodos, newTodoItem]);
+  //     setnewTodo('');
+  //   }
+  // };
+
+  // const undoTodoHandler = (todoId) =>{
+  //   return;
+  // }
+
+  // const toggleTodoHandler = (todoId) => {
+  //   return;
+  // }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="titleBox">
+        <h1 className="titleName">To-Do_List</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="boxlist">
+        {/* <form>
+          <input type='text'>Enter here</input>
+          <input type="button"/>
+        </form> */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
